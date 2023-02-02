8d0646b1e26556cfad1b364a833418b7dd38e7f6
Kartik <kartikkedia011@gmail.com>
2023-02-02 22:08:58 +0530

    css commit

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,22 +1,23 @@
 import React from 'react'
-import { GoogleOutlined} from '@ant-design/icons'
+import { GoogleOutlined } from '@ant-design/icons'
 import { auth } from './firebase';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
+import { FcGoogle } from 'react-icons/fc';
 
 export default function Login() {
     return (<div id='login-page'>
         <div id='login-card'>
             <h1>WELCOME TO SOCIDESK</h1>
+
+
+            <div className='login-button google'
+                onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
+
+                <FcGoogle /> Sign In With Google
+
+            </div>
         </div>
-        
-        <div className='login-button google'
-            onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
-
-            <GoogleOutlined /> Sign In With Google
-
-        </div>
-
     </div>)
 }
